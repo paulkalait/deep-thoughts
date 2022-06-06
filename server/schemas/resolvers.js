1d9ff8aa550a35c342d32b8e5d0cfae1cdfc4f3c
Your Name <kalaitzidispaul@gmail.com>
2022-06-06 17:51:03 -0400

    added queries and resolvers for users,  user, and thoughts, thought

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,32 @@
+const { User, Thought } = require('../models')
+
 // we need to set up the resolver that will serve the response for the typeDef query 
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello World'
-        }
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
+          },
+          //finding a single though 
+          thought: async (parent, { _id}) => {
+              return Thought.findOne({_id});
+          },
+          //get all users
+          users: async() => {
+              return User.find()
+              .select('-__v -password')
+              .populate('friends')
+              .populate('thoughts')
+          },
+          //get a user by username
+          user: async (parent, { username}) => {
+            return User.findOne({ username})
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts')
+          }
     }
+    
 }
 
 module.exports = resolvers

ebe40cf07688bc1a77eee0e601a28c83133132f6
Your Name <kalaitzidispaul@gmail.com>
2022-06-09 17:26:26 -0400

    fixed profile route

--- client/src/App.js
+++ client/src/App.js
@@ -48,6 +48,7 @@ function App() {
               <Route path='/login' element={<Login />} />
               <Route path='/signup' element={<Signup />} />
               <Route path='/profile/:username' element={<Profile />} />
+              <Route path='/profile/' element={<Profile />} />
               <Route path="/thought/:id" element={<SingleThought />} />
               <Route path="*" element={<NoMatch />} />
             </Routes>
